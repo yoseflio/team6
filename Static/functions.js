2d161533931d7e12e9b9aab942ad0c0dedeaf62a
danashkol <danashkol@gmail.com>
2022-05-19 19:02:12 +0300

    dana

---
+++ Static/functions.js
@@ -0,0 +1,10 @@
+function categoryName(){
+    var categories = document.getElementsByClassName("pic");
+    console.log(categories.length);
+    for(var i = 0; i < categories.length; i++){
+        var textOf = "categories[i].className";
+        console.log(textOf);
+        categories[i].style.setProperty('--className',textOf); 
+
+    }
+}

183a3b93ab925b5c84c86b9d7287268ecac12359
noamArbes <noamarbes1@gmail.com>
2022-05-23 15:45:04 +0300

    first cake page

--- Static/functions.js
+++ Static/functions.js
@@ -2,7 +2,7 @@ function categoryName(){
     var categories = document.getElementsByClassName("pic");
     console.log(categories.length);
     for(var i = 0; i < categories.length; i++){
-        var textOf = categories[i].className;
+        var textOf = "categories[i].className";
         console.log(textOf);
         categories[i].style.setProperty('--className',textOf); 
 

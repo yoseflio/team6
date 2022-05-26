9ff61878cad319b2fb0396852c90e7661688eb1f
danashkol <danashkol@gmail.com>
2022-05-26 18:26:24 +0300

    dana

--- Static/functions.js
+++ Static/functions.js
@@ -2,21 +2,23 @@ function categoryName(){
     var categories = document.getElementsByClassName("pic");
     console.log(categories.length);
     for(var i = 0; i < categories.length; i++){
-        var textOf = "categories[i].className";
+        var textOf = categories[i].className;
         console.log(textOf);
         categories[i].style.setProperty('--className',textOf); 
 
     }
 }
 
-function openPopUp(src){
+function openPopUp(s, cakeName){
     var popUp = document.getElementById("container");
     var left = document.getElementById("left");
     var photosCategory = document.getElementById("photosCategory");
-    var body = document.getElementById("body");
-    console.log(popUp);
+    var name = document.getElementById("name");
     popUp.classList.add("open-popUp");
     left.classList.add("open-left");
-    photosCategory.classList.add("open-photoCategory")
-    body.classList.add("open-body");
+    
+    photosCategory.classList.add("open-photoCategory");
+    name.innerHTML = cakeName;
+    document.getElementById("oleft").src = s;
+    console.log(document.getElementById("oleft").src);
 }

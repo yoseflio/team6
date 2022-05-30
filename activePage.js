86271ee5326bc96660206e0e61f36a3cdadeb9c2
unknown <yoseflio@post.bgu.ac.il>
2022-05-30 14:11:07 +0300

    word

--- activePage.js
+++
@@ -1,7 +0,0 @@
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('nav a').forEach(link => {
-  if(link.href.includes(`${activePage}`)){
-    link.classList.add('active');
-    console.log(activePage);
-  }
-})
